525c176e80d67b14e050664d57bb172e8705ab24
fengshun <fengshun58@163.com>
2018-12-30 23:19:44 +0800

    test

--- calu.py
+++ calu.py
@@ -18,6 +18,7 @@ from sympy import *
 lmd = Symbol('lmd')
 cw = Symbol('cw')
 n = Symbol('n')
+
 f = 1-((2*n*exp(-lmd)/((cw-1)*(-exp(-lmd))+2))+n*(n-1)*4*exp(-2*lmd)/(2*(((cw-1)*exp(-lmd)+2)**2))*(cw*exp(-lmd)+2*n-1+(3-2*n)*(1-exp(-lmd)))/(cw*exp(-lmd)+2-3*exp(-lmd)))
 #print diff(f,cw)
 # -2*n*(n - 1)*exp(-3*lmd)/(((cw - 1)*exp(-lmd) + 2)**2*(cw*exp(-lmd) + 2 - 3*exp(-lmd))) + 2*n*(n - 1)*(cw*exp(-lmd) + 2*n + (1 - exp(-lmd))*(-2*n + 3) - 1)*exp(-3*lmd)/(((cw - 1)*exp(-lmd) + 2)**2*(cw*exp(-lmd) + 2 - 3*exp(-lmd))**2) + 4*n*(n - 1)*(cw*exp(-lmd) + 2*n + (1 - exp(-lmd))*(-2*n + 3) - 1)*exp(-3*lmd)/(((cw - 1)*exp(-lmd) + 2)**3*(cw*exp(-lmd) + 2 - 3*exp(-lmd))) - 2*n*exp(-2*lmd)/(-(cw - 1)*exp(-lmd) + 2)**2
